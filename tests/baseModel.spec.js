670597ef5c417e22f964fa0a85ae9b4a1e660915
Stephen Wan <stephen@stephenwan.net>
2014-11-22 15:46:45 -0600

    support for get/set on model properties

--- tests/baseModel.spec.js
+++ tests/baseModel.spec.js
@@ -24,4 +24,35 @@ describe('BaseModel', () => {
 
   });
 
+  coIt('should set a value on the model', function *() {
+
+    class TestModel extends BaseModel {
+      get definition() {
+        return {
+          test: 'is a thing'
+        };
+      }
+    }
+
+    var createdModel = yield TestModel.generate();
+    assert.notEqual(createdModel, null, 'generated model is null');
+
+    var newToken = createdModel.token;
+
+    var loadedModel = yield TestModel.load(createdModel.token);
+    assert.equal(loadedModel.token, newToken, 'loaded model token does not match');
+
+    loadedModel.test = 'hello';
+    yield loadedModel.save();
+
+    loadedModel = yield TestModel.load(createdModel.token);
+    console.log(loadedModel.test);
+    assert.equal(loadedModel.test, 'hello', 'loaded model test value does not match');
+
+    yield loadedModel.delete();
+    var deletedModel = yield TestModel.load(loadedModel.token);
+    assert.strictEqual(deletedModel, null, 'model not deleted');
+
+  });
+
 });
