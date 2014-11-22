530f37406de49dbe9f0870bceb6bcca0c54172dd
Stephen Wan <stephen@stephenwan.net>
2014-11-22 14:31:51 -0600

    formatting

--- tests/baseModel.spec.js
+++ tests/baseModel.spec.js
@@ -10,17 +10,17 @@ describe('BaseModel', () => {
 
   coIt('should create, load, then delete model', function *() {
 
-      var createdModel = yield BaseModel.generate();
-      assert.notEqual(createdModel, null, 'generated model is null');
+    var createdModel = yield BaseModel.generate();
+    assert.notEqual(createdModel, null, 'generated model is null');
 
-      var newToken = createdModel.token;
+    var newToken = createdModel.token;
 
-      var loadedModel = yield BaseModel.load(createdModel.token);
-      assert.equal(loadedModel.token, newToken, 'loaded model token does not match');
+    var loadedModel = yield BaseModel.load(createdModel.token);
+    assert.equal(loadedModel.token, newToken, 'loaded model token does not match');
 
-      yield loadedModel.delete();
-      var deletedModel = yield BaseModel.load(loadedModel.token);
-      assert.strictEqual(deletedModel, null, 'model not deleted');
+    yield loadedModel.delete();
+    var deletedModel = yield BaseModel.load(loadedModel.token);
+    assert.strictEqual(deletedModel, null, 'model not deleted');
 
   });
 
